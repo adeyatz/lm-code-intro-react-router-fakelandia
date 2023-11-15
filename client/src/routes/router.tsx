c4362ace06292eabf6bd37d47c66bf889beac46c
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-11-15 18:57:48 +0000

    fix: edit title tag in index.html

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../components/home";
 import Confession from "../components/confession";
 import Misdemeanour from "../components/misdemeanour";
+import NotFound from "../components/not-found";
 
 export const Router: React.FC = () => (
   <Routes>
