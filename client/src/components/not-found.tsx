157600aa0acae9aeee2b253182790d1a30518233
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-11-15 19:06:44 +0000

    feat: render url in NotFound component

--- client/src/components/not-found.tsx
+++ client/src/components/not-found.tsx
@@ -1,2 +1,7 @@
-const NotFound: React.FC = () => <>Page Not Found!</>;
+import { useLocation } from "react-router-dom";
+const NotFound: React.FC = () => {
+  const url = useLocation();
+
+  return <>Error 404: {url.pathname} :Page Not Found!</>;
+};
 export default NotFound;
