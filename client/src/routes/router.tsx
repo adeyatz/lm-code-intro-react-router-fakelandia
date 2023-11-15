335d2444ca7f75104e5372cdb1f7ed7b16a75ed7
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-11-15 18:48:07 +0000

    chore:rename router.ts to router.tsx

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -1,9 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../components/home";
 import Confession from "../components/confession";
 import Misdemeanour from "../components/misdemeanour";
 
-const Router: React.FC = () => (
+export const Router: React.FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/misdemeanour" element={<Misdemeanour />} />
