12a10cb536f9b0ceea9092b619a44653c36ce837
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-11-16 16:38:10 +0000

    refactor: nest Routes in Layout in routes.tsx

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -3,13 +3,16 @@ import Home from "../components/home";
 import Confession from "../components/confession";
 import Misdemeanour from "../components/misdemeanour";
 import NotFound from "../components/not-found";
+import Layout from "../components/layout";
 
 export const Router: React.FC = () => (
   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/misdemeanour" element={<Misdemeanour />} />
-    <Route path="/confession" element={<Confession />} />
-    <Route path="*" element={<NotFound />} />
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="/misdemeanour" element={<Misdemeanour />} />
+      <Route path="/confession" element={<Confession />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
   </Routes>
 );
 
