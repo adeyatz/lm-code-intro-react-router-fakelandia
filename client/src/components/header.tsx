4338eeeae68162694243133f1b41f33bfef0fe32
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-11-17 11:04:21 +0000

    style: Adjust the layout of Heeader component

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -3,17 +3,21 @@ import Nav from "./nav";
 const Header: React.FC = () => {
   return (
     <>
-      Fakelandia
-      <br />
-      Justice
-      <br />
-      Department
-      <img
-        src="/src/assets/fakelandia.png"
-        alt="Here should be a header image"
-        height={50}
-        width={300}
-      />
+      <header>
+        <span className="fjp-title">
+          Fakelandia
+          <br />
+          Justice
+          <br />
+          Department
+        </span>
+        <img
+          src="/src/assets/fakelandia.png"
+          alt="Here should be a header image"
+          height={50}
+          width={300}
+        />
+      </header>
       <Nav />
     </>
   );
