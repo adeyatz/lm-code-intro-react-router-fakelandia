0337c5b3ae62d5cdb231ebcc8b89727370ef28ac
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-12-07 14:23:41 +0000

    test: App.test.tsx - fails when navigating to Misdemeanour page

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,6 +1,7 @@
-import "@testing-library/jest-dom";
+// import "@testing-library/jest-dom";
+// import "happy-dom";
 import { render, screen } from "@testing-library/react";
-// import { fireEvent } from "@testing-library/react";
+import useEvent from "@testing-library/user-event";
 import App from "./App";
 
 test("demo", () => {
@@ -11,6 +12,64 @@ test("Renders the main page, expect 1 image", async () => {
   render(<App />);
   const images = screen.getAllByRole(`img`);
   expect(images.length).toBe(1);
+});
+
+test("Renders the main page, expect home component to be visible", async () => {
+  const { getByText } = render(<App />);
+
+  expect(getByText(/Welcome to the home/)).toBeInTheDocument();
+});
+
+test("tab test from testing-library.com", async () => {
+  const user = useEvent.setup();
+  render(
+    <div>
+      <input type="checkbox" />
+      <input type="radio" />
+      <input type="number" />
+    </div>
+  );
+
+  const checkbox = screen.getByRole("checkbox");
+  const radio = screen.getByRole("radio");
+  const number = screen.getByRole("spinbutton");
+
+  expect(document.body).toHaveFocus();
+
+  await user.tab();
+
+  expect(checkbox).toHaveFocus();
+
+  await user.tab();
+
+  expect(radio).toHaveFocus();
+
+  await user.tab();
+
+  expect(number).toHaveFocus();
+
+  await user.tab();
+
+  // cycle goes back to the body element
+  expect(document.body).toHaveFocus();
+
+  await user.tab();
+
+  expect(checkbox).toHaveFocus();
+});
+
+test("Renders the main page, user clicks Misdemeanours NavLink, expect Misdemeanours component to be visible", async () => {
+  const user = useEvent.setup();
+
+  const { getByText } = render(<App />);
+
+  expect(getByText(/Welcome to the home/)).toBeInTheDocument();
+
+  const navLink = getByText(/Misdemeanours/);
+  console.log(navLink.getAttribute("href") + "---" + navLink.innerText);
+  await user.click(navLink);
+  // TODO why does this not work!!!!
+  expect(screen.getByText(/Misdemeanour!/)).toBeDefined();
 });
 
 /*
