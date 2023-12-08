339a436ae1379d11f4468a6d5a3c6bac255b165e
Adrian Yates <118460049+adeyatz@users.noreply.github.com>
2023-12-08 18:33:07 +0000

    feat: ConfessionReason populate dropdown list with misdemeanours and Just-talk options

--- client/src/components/pages/confession-reason.tsx
+++ client/src/components/pages/confession-reason.tsx
@@ -1,8 +1,19 @@
-const ConfessionReason: React.FC = () => (
-  <label>
-    Reason
-    <input id="reason" type="text" />
-  </label>
-);
+import { MISDEMEANOURS, JUST_TALK } from "../../types/misdemeanours.types";
+
+const ConfessionReason: React.FC = () => {
+  const reasons = [...MISDEMEANOURS, JUST_TALK];
+  return (
+    <label>
+      Reason
+      <select>
+        {reasons.map((reason, index) => (
+          <option key={index} value={reason}>
+            {reason}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+};
 
 export default ConfessionReason;
